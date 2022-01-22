6de9d9cda96bd2062818e1842dd825e4187f0725
Fischer Wells <fisch.wells@gmail.com>
2022-01-21 19:39:28 -0700

    new unity project; it actually works

--- Scripting Proj 1/Assets/script4.cs
+++ Scripting Proj 1/Assets/script4.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class 1stscriptbb : MonoBehaviour
+public class script4 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("ground control to major tom");
     }
 
     // Update is called once per frame
