ed0eea44e7f1d86f8960d046e55c8d7a7b4bef47
Fischer Wells <fisch.wells@gmail.com>
2022-01-25 23:13:19 -0700

    work from 1/24 & 1/25

---
+++ Scripting Proj 1/ScriptingProj1/Assets/scripts/Assignments/Variables-Conditionals/Variables.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+    //Global Variables
+    
+{
+   public int money;
+   public int health;
+   public float water;
+   public int ammunition;
+   public bool isDay = true;
+   public string LoginName = "wakaflocka";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        money = 175;
+        Debug.Log("wallet: $" + money);
+        health = 100;
+        ammunition = 250;
+        Debug.Log("Ammo:" + ammunition);
+        water = 128f;
+        Debug.Log("water (oz.)" + water);
+        if(LoginName == "wakaflocka")
+        {
+            Debug.Log("logged in and ready to roll");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDay)
+        {
+            Debug.Log("the sun is up");
+        }
+        else if (isDay == false)
+        {
+            Debug.Log("the sun is down");
+        }
+        if (water <= 49.9)
+        {
+            Debug.Log("drink up");
+        }
+        if(ammunition <= 40)
+        {
+            Debug.Log("reload");
+        }
+        if(money < 60)
+        {
+            Debug.Log("ur poor brother");
+        }
+        if (health >= 100)
+        {
+            Debug.Log("you are in perfect health");
+
+        }
+        else if (health <= 75 - 99)
+        {
+            Debug.Log("you are doing fine");
+        }
+        else if(health <= 50 - 74)
+        {
+            Debug.Log("you are not doing as fine");
+        }
+        else if(health <= 25 - 49)
+        {
+            Debug.Log("you are definitely now in bad shape");
+        }
+        else if(health <= 1 - 24)
+        {
+            Debug.Log("this is the end");
+        }
+    }
+}
--- Scripting Proj 1/ScriptingProj1/Assets/scripts/howdy.cs
+++ Scripting Proj 1/ScriptingProj1/Assets/scripts/howdy.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class howdy : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        print("Ground Control to Major Tom");
+        Debug.Log("howdy gamers");
     }
 
     // Update is called once per frame
