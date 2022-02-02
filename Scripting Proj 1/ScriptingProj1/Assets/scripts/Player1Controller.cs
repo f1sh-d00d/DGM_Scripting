62d45974a238b955cb01dec7f94f6adb4a86c23f
Fischer Wells <fisch.wells@gmail.com>
2022-02-01 23:25:54 -0700

    start of movement

--- Scripting Proj 1/ScriptingProj1/Assets/scripts/Player1Controller.cs
+++ Scripting Proj 1/ScriptingProj1/Assets/scripts/Player1Controller.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player1Controller : MonoBehaviour
 {
+    public float speed = 35.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class Player1Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
