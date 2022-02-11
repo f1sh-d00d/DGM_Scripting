d8276cb1a00230d4f240c191212c6fa846220797
Fischer Wells <fisch.wells@gmail.com>
2022-02-10 21:49:48 -0700

    balloon behavior when clicked

---
+++ balloon game/BalloonGame/Assets/Scenes/Assignments/scripts/balloonController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class balloonController : MonoBehaviour
+{
+    //variables
+    public int scoreIncrease = 100;
+    public int clicksToPop = 3;
+    public int totalScore;
+    public float scaleUp = .25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //score stuff maybe
+        // totalScore = 0;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+    }
+
+    //balloon when clicked
+    private void OnMouseDown()
+    {
+        clicksToPop -= 1;
+
+        transform.localScale += Vector3.one * scaleUp;
+
+        //probably wrong score function
+        //totalScore = totalScore + scoreIncrease;
+        
+        if(clicksToPop == 0)
+        {
+            Destroy(gameObject);
+        }
+        
+
+    }
+    
+}
