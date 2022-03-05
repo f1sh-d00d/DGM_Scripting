8cb17a1ea6429676253b8c60c8b42872d49e580c
Fischer Wells <fisch.wells@gmail.com>
2022-03-05 12:27:23 -0700

    started rougelike

---
+++ Roguelike Prototype/Assets/Scripts/playerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float moveSpeed = 5.0f; //speed of player movement
+    private Rigidbody2D rb; //references rigidbody component
+    Vector2 movement; //stores players x,y pos movement
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal"); // input for L R movement
+        movement.y = Input.GetAxis("Vertical"); // input for Up Down movement
+    }
+    void FixedUpdate()
+    {
+        //Apply physics and move character
+
+        rb.MovePosition(rb.position+movement*moveSpeed*Time.deltaTime);
+
+
+    }
+}
