a0e9cbcb2688365ead38a55ea5b14889c20aad56
f1sh-d00d <97486370+f1sh-d00d@users.noreply.github.com>
2022-03-06 23:04:42 -0700

    Update playerController.cs

--- Roguelike Prototype/Assets/Scripts/playerController.cs
+++ Roguelike Prototype/Assets/Scripts/playerController.cs
@@ -11,7 +11,7 @@ public class playerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>(); // references Rigidbody2D component
 
     }
 
